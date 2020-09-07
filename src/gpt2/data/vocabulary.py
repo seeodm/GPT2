31b28196086580e6709ac5841be109153a96ec1e
seeodm <sdm0325@gmail.com>
2020-09-07 15:36:44 +0900

    [vocab] Google Sentencepiece Vocab 대응

--- src/gpt2/data/vocabulary.py
+++ src/gpt2/data/vocabulary.py
@@ -7,24 +7,29 @@ class Vocab(object):
                  unk_token: str = '<unk>',
                  bos_token: str = '<s>',
                  eos_token: str = '</s>',
-                 pad_token: str = '<pad>'):
+                 pad_token: str = '<pad>',
+                 cls_token: str = '[CLS]',
+                 sep_token: str = '[SEP]'):
         self.unk_token = unk_token
         self.bos_token = bos_token
         self.eos_token = eos_token
         self.pad_token = pad_token
+        self.cls_token = cls_token
+        self.sep_token = sep_token
 
         with open(vocab_path, 'r', encoding='utf-8') as fp:
-            self.additional_tokens = [bos_token, eos_token, pad_token]
-
             # The additional tokens would be inserted before the words.
-            self.words = self.additional_tokens + fp.read().split()
+            self.words = fp.read().split()[::2]
             self.vocab = {word: i for i, word in enumerate(self.words)}
 
     def __getitem__(self, idx_or_token: Union[int, str]) -> Union[str, int]:
-        if isinstance(idx_or_token, str):
-            return self.vocab[idx_or_token]
-        else:
-            return self.words[idx_or_token]
+        try:
+            if isinstance(idx_or_token, str):
+                return self.vocab[idx_or_token]
+            else:
+                return self.words[idx_or_token]
+        except:
+            return self.vocab[self.unk_token]
 
     def __contains__(self, token: str) -> bool:
         return token in self.words
@@ -49,3 +54,11 @@ class Vocab(object):
     @property
     def pad_idx(self) -> int:
         return self.vocab[self.pad_token]
+
+    @property
+    def cls_idx(self) -> int:
+        return self.vocab[self.cls_token]
+
+    @property
+    def sep_idx(self) -> int:
+        return self.vocab[self.sep_token]
